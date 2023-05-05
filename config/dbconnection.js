57aeba27a555d61e8ed40aff555a1354bade99e8
Fernando Augusto Ortolan <fernando.ao2000@aluno.ifsc.edu.br>
2023-05-05 15:18:53 -0300

    Establish connection to the database

---
+++ config/dbconnection.js
@@ -0,0 +1,17 @@
+const sequelize = require("sequelize");
+
+const connection = new sequelize("dbatividade", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  define: {
+    timestamps: false,
+    freezeTableName: true
+  }
+});
+
+try {
+  connection.authenticate();
+  console.log("Conexão com o banco de dados estabelecida com sucesso.")
+} catch (error) {
+  console.error(`Não foi possível conectar ao banco de dados: ${error}`);
+}
